bd01dffa3a895b186ce5515f7f4acaff251f6cb0
Josh Chappell <joshua.chappell@gmail.com>
2018-08-23 17:50:02 -0400

    Start tests for the server itself.

---
+++ Lighthouse.Server.Tests/LighthouseServerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lighthouse.Server.Tests
+{
+    public class LighthouseServerTests
+    {
+		[Fact]
+		[Trait("Type", "Deployment")]
+		public void FindServices_DirectoryLoading_FoundApps()
+		{
+			var servicesToRun = LighthouseLauncher
+				.FindServices(new ILighthouseAppLocation[]
+					{
+						new LighthouseFileSystemLocation { Directory = $"{Environment.CurrentDirectory}\\Apps" }
+					}, logHandler: (o, m) => Output.WriteLine($"{o}: {m}")
+				);
+
+			servicesToRun.Count().Should().Be(1);
+		}
+	}
+}
