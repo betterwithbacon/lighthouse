1dc96f7815aa2b087760bd16a08a716c9eb6674c
Josh Chappell <joshua.chappell@gmail.com>
2019-04-25 16:04:28 -0400

    started to add a ping service

---
+++ Lighthouse.Core/Services/PingService.cs
@@ -0,0 +1,32 @@
+﻿using Lighthouse.Core.Configuration.ServiceDiscovery;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lighthouse.Core.Services
+{
+    [ExternalLighthouseService("ping")]
+    public class PingService : ILighthouseService
+    {
+        public string Id => throw new NotImplementedException();
+
+        public LighthouseServiceRunState RunState => throw new NotImplementedException();
+
+        public ILighthouseServiceContainer Container => throw new NotImplementedException();
+
+        public void Initialize(ILighthouseServiceContainer context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
