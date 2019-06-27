d7f9ff9d250689cd1595546b3020c850dcf0a43a
Josh Chappell <joshua.chappell@gmail.com>
2019-06-27 14:31:04 -0400

    stubbed warehouseserver

---
+++ Lighthouse.Apps.WarehouseServer/WarehouseServer.cs
@@ -0,0 +1,10 @@
+﻿using Lighthouse.Core.Configuration.ServiceDiscovery;
+using System;
+
+namespace Lighthouse.Apps.WarehouseServer
+{
+    [ExternalLighthouseService("warehouse")]
+    public class WarehouseServer
+    {
+    }
+}
