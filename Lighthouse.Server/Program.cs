4f23c18d52e74cda457ad9f769ec6d9ae694bcda
Josh Chappell <joshua.chappell@gmail.com>
2019-06-27 13:46:42 -0400

    got run/view options working

--- Lighthouse.Server/Program.cs
+++ Lighthouse.Server/Program.cs
@@ -9,36 +9,60 @@ namespace Lighthouse.Server
 {
     class Program
     {
-        public class Options
+        [Verb("run")]
+        public class RunOptions
         {
             [Option('a', "app", Required = true, HelpText = "Application name to run")]
             public string Application { get; set; }
+        }
+
+        [Verb("view")]
+        public class ViewOptions
+        {
+            [Option('a', "app", Required = true, HelpText = "Application name to run")]
+            public string Application { get; set; }
+
+            [Option('s', "server", Required = true, HelpText = "Server application is hosted on", Default ="127.0.0.1:50505")]
+            public string Server { get; set; }
         }
 
         static async Task Main(string[] args)
         {
             Console.WriteLine("Starting lighthouse server");
 
-            var appName = "";
-
-            Parser.Default.ParseArguments<Options>(args)
-               .WithParsed<Options>(o =>
-               {
-                   appName = o.Application;
-               });
-
-            var server = new LighthouseServer(
-                localLogger: Console.WriteLine);
-
-            Type appType = LighthouseFetcher.Fetch(appName);
-
-            if(appType == null)
-            {
-                throw new ApplicationException("Can't find app name");
-            }
-
+            var server = new LighthouseServer(localLogger: Console.WriteLine);
             server.Start();
-            server.Launch(appType);
+
+            Parser.Default.ParseArguments<RunOptions, ViewOptions>(args)
+                .MapResult(
+                    (RunOptions runOptions) =>
+                    {
+                        Type appType = LighthouseFetcher.Fetch(runOptions.Application);
+                        if (appType == null)
+                        {
+                            throw new ApplicationException("Can't find app name");
+                        }
+
+                        server.Launch(appType);
+                        return 0;
+                    },
+                    (ViewOptions viewOptions) =>
+                    {
+                        if(Uri.TryCreate(viewOptions.Server, UriKind.Absolute, out var uri))
+                        {
+                            var connection = server.Connect(uri);
+
+                            // do more things here
+                            return 0;
+                        }
+                        else
+                        {
+                            throw new ApplicationException($"URI: {viewOptions.Server} could not be converted to a valid URI");                            
+                        }
+                    },
+                    errs => 1
+                );
+               
             _ = Console.ReadLine();
             await server.Stop();
         }
