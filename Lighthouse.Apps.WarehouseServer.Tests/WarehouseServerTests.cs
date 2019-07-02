393fc898607695532ac3353cf8898a6727f4552d
Josh Chappell <joshua.chappell@gmail.com>
2019-07-02 17:40:35 -0400

    More stuff in warehouseServer

--- Lighthouse.Apps.WarehouseServer.Tests/WarehouseServerTests.cs
+++ Lighthouse.Apps.WarehouseServer.Tests/WarehouseServerTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+using Lighthouse.Core.Storage;
 using System;
 using Xunit;
 
@@ -6,9 +8,63 @@ namespace Lighthouse.Apps.WarehouseServer.Tests
     public class WarehouseServerTests
     {
         [Fact]
-        public void Handle_WithStorageRequest_Returns_Successful()
+        public void Handle_WithStorageRequest_Retrieve_Returns_Successful()
         {
+            var mockWarehouse = NSubstitute.Substitute.For<IWarehouse>();
+            WarehouseServer server = new WarehouseServer(mockWarehouse);
 
+            var request = new StorageRequest
+            {
+                Action = StorageAction.Retrieve
+            };
+
+            var response = server.Handle(request);
+            response.WasSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Handle_WithStorageRequest_Store_Returns_Successful()
+        {
+            var mockWarehouse = NSubstitute.Substitute.For<IWarehouse>();
+            WarehouseServer server = new WarehouseServer(mockWarehouse);
+
+            var request = new StorageRequest
+            {
+                Action = StorageAction.Store
+            };
+
+            var response = server.Handle(request);
+            response.WasSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Handle_WithStorageRequest_Inspect_Returns_Successful()
+        {
+            var mockWarehouse = NSubstitute.Substitute.For<IWarehouse>();
+            WarehouseServer server = new WarehouseServer(mockWarehouse);
+
+            var request = new StorageRequest
+            {
+                Action = StorageAction.Inspect
+            };
+
+            var response = server.Handle(request);
+            response.WasSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Handle_WithStorageRequest_Delete_Returns_Successful()
+        {
+            var mockWarehouse = NSubstitute.Substitute.For<IWarehouse>();
+            WarehouseServer server = new WarehouseServer(mockWarehouse);
+
+            var request = new StorageRequest
+            {
+                Action = StorageAction.Delete
+            };
+
+            var response = server.Handle(request);
+            response.WasSuccessful.Should().BeTrue();
         }
     }
 }
--- Lighthouse.Apps.WarehouseServer/WarehouseServer.cs
+++ Lighthouse.Apps.WarehouseServer/WarehouseServer.cs
@@ -11,31 +11,79 @@ namespace Lighthouse.Apps.WarehouseServer
     [ExternalLighthouseService("warehouse")]
     public class WarehouseServer : LighthouseServiceBase, IRequestHandler<StorageRequest, StorageResponse>
     {
-        public WarehouseServer()
+        public WarehouseServer(IWarehouse warehouse)
         {
+            Warehouse = warehouse;
         }
 
         public IList<Type> Consumes => throw new NotImplementedException();
 
-        public Warehouse Warehouse { get; private set; }
+        public IWarehouse Warehouse { get; private set; }
 
         public StorageResponse Handle(StorageRequest request)
         {
+            if(Warehouse == null)
+            {
+                throw new ApplicationException("No warehouse is present");
+            }
+
             switch (request.Action)
             {
                 case StorageAction.Store:
-                    Warehouse.Store(StorageScope.Global, request.Key, request.Data, request.LoadingDockPolicies);
-                    return StorageResponse.Stored;
+                    return Store(request);
                 case StorageAction.Retrieve:
-                    Warehouse.Retrieve(StorageScope.Global, request.Key);
-                    return new StorageResponse(false, "");
+                    return Retrieve(request);                    
                 case StorageAction.Inspect:
-                    return new StorageResponse(false, "");
+                    return Inspect(request);
                 case StorageAction.Delete:
-                    return new StorageResponse(false, "");
+                    return Delete(request);
                 default:
                     return new StorageResponse(false, "");
             }
+        }
+
+        private StorageResponse Delete(StorageRequest request)
+        {
+            if (request.PayloadType == StoragePayloadType.Blob)
+                Warehouse.Store(StorageScope.Global, request.Key, null, request.LoadingDockPolicies);
+            else
+                Warehouse.Store(StorageScope.Global, request.Key, null, request.LoadingDockPolicies);
+
+            return new StorageResponse();
+        }
+
+        private StorageResponse Inspect(StorageRequest request)
+        {
+            var manifest = Warehouse.GetManifest(StorageScope.Global, request.Key);
+
+            return new StorageResponse
+            {
+                Manifest = manifest
+            };
+        }
+
+        private StorageResponse Retrieve(StorageRequest request)
+        {
+            string value = Warehouse.Retrieve(StorageScope.Global, request.Key);
+
+            return new StorageResponse
+            {
+                StringData = value
+            };
+        }
+
+        private StorageResponse Store(StorageRequest request)
+        {
+            Receipt receipt = null;
+            if (request.PayloadType == StoragePayloadType.Blob)
+                receipt = Warehouse.Store(StorageScope.Global, request.Key, request.Data, request.LoadingDockPolicies);
+            else
+                receipt = Warehouse.Store(StorageScope.Global, request.Key, request.StringData, request.LoadingDockPolicies);
+
+            return new StorageResponse
+            {
+                Receipt = receipt
+            };
         }
 
         protected override void OnStart()
@@ -61,12 +109,18 @@ namespace Lighthouse.Apps.WarehouseServer
 
         public bool WasSuccessful { get; }
         public string Message { get; }
+        public Receipt Receipt { get; internal set; }
+        public byte[] Data { get; set; }
+        public string StringData { get; set; } // TODO: is this a necessary hack?!
+        public StorageKeyManifest Manifest { get; internal set; }
     }
 
     public class StorageRequest
     {
+        public StoragePayloadType PayloadType { get; set; }
         public StorageAction Action { get; set; }
         public byte[] Data { get; set; }
+        public string StringData { get; set; } // TODO: is this a necessary hack?!
         public string Key { get; set; }
         public IEnumerable<StoragePolicy> LoadingDockPolicies { get; set; }
     }
@@ -77,5 +131,11 @@ namespace Lighthouse.Apps.WarehouseServer
         Retrieve,
         Delete,
         Inspect
+    }
+
+    public enum StoragePayloadType
+    {
+        String,
+        Blob
     }
 }
