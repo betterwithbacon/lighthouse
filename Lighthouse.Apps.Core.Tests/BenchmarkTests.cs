8c9e224c621782eb93aaa138f44a10db767f484b
betterwithbacon <joshua.chappell@gmail.com>
2019-11-12 17:07:24 -0500

    benchmarking

--- Lighthouse.Apps.Core.Tests/BenchmarkTests.cs
+++ Lighthouse.Apps.Core.Tests/BenchmarkTests.cs
@@ -1,4 +1,7 @@
 using System;
+using FluentAssertions;
+using Lighthouse.Core;
+using NSubstitute;
 using Xunit;
 
 namespace Lighthouse.Apps.Core.Tests
@@ -8,10 +11,34 @@ namespace Lighthouse.Apps.Core.Tests
         [Fact]
         public void NodeLatencyBenchmark_Run_NoTargets_Returns_Zero()
         {
-            var benchmark = new NodeLatencyBenchmark();
-            var result = benchmark.Run();
 
-            result.
+            var testContainer = Substitute.For<ILighthouseServiceContainer>();
+            var benchmark = new NodeLatencyBenchmark(testContainer, "127.0.0.1" );
+            var result = benchmark.RunBenchmark();
+
+            result.Should().Be(0);
         }
     }
+
+    public class BenchmarkRunnerTests
+    {
+        [Fact]
+        public void NodeLatencyBenchmark_Run_NoTargets_Returns_Zero()
+        {
+
+            var benchmark = new ZeroTimeBenchark();
+            var result = benchmark.RunBenchmark();
+
+            result.Should().Be(0);
+        }
+    }
+
+    internal class ZeroTimeBenchark : IBenchmark
+    {
+        public void Run()
+        {
+            // do absolutely nothing
+        }
+    }
+
 }
--- Lighthouse.Apps.Core/BenchmarkApp.cs
+++ Lighthouse.Apps.Core/BenchmarkApp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Lighthouse.Core;
 using Lighthouse.Core.Configuration.ServiceDiscovery;
@@ -24,21 +26,86 @@ namespace Lighthouse.Apps.Core
 
         public BenchmarkTarget Target { get; set; }
 
-        protected override Task OnStart()
+        public IDictionary<string, string> Args {get;set;}
+
+        private IBenchmark Benchmark { get; set; }
+
+
+        protected override async Task OnStart()
         {
-            // what is being benchmarked
+            Benchmark = CreateBenchmark(Type, Target);
+            await Task.Run(() => {
+                    Benchmark.RunBenchmark();
+                });
+        }
 
+        private IBenchmark CreateBenchmark(BenchmarkType benchmarkType, BenchmarkTarget target)
+        {
+            if(benchmarkType == BenchmarkType.Latency)
+            {
+                if (target == BenchmarkTarget.Node)
+                {
+                    return new NodeLatencyBenchmark(this.Container, Args["targetIp"]);
+                }
+            }
 
+            return null;
         }
     }
 
     public class NodeLatencyBenchmark : IBenchmark
     {
+        private Uri TargetUri { get; }
+        private ILighthouseServiceContainer Container { get; }
 
+        public NodeLatencyBenchmark(ILighthouseServiceContainer container, string targetIp)
+        {
+            this.Container = container;
+
+            if(!Uri.TryCreate(targetIp, UriKind.Absolute, out var parsedUri))
+            {
+                TargetUri = parsedUri;
+            }
+            else
+            {
+                throw new Exception("Invalid target IP address.");
+            }
+        }
+
+        public void Run()
+        {
+            // test latency, by sendign a request o the remote target, and validating the response
+
+            // connect based on just name and not IP
+            var connection = Container.Connect(TargetUri);
+
+            if (!connection.TryConnect().GetAwaiter().GetResult())
+            {
+                throw new Exception("Connection failed to be created.");
+            }
+        }
     }
 
     public interface IBenchmark
     {
+        void Run();
+    }
 
+    public static class BenchmarkRunner
+    {
+        /// <summary>
+        /// Returns the total runtime in milliseconds that the benchmark took to run
+        /// </summary>
+        /// <param name="benchmark"></param>
+        /// <returns></returns>
+        public static long RunBenchmark(this IBenchmark benchmark)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            benchmark.Run();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
     }
 }
