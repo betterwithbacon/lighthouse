0b620f26bf3bd3dbd266240cc5a5279aea8c7bfb
betterwithbacon <joshua.chappell@gmail.com>
2019-11-15 16:58:02 -0500

    more stubs for virtual network

--- Lighthouse.Core/Hosting/VirtualNetwork.cs
+++ Lighthouse.Core/Hosting/VirtualNetwork.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lighthouse.Core.IO;
+
 namespace Lighthouse.Core.Hosting
 {
-    public class VirtualNetwork
+    public class VirtualNetwork : INetworkProvider
     {
-        public VirtualNetwork()
+        public Dictionary<Uri, ILighthouseServiceContainer> Containers { get; } = new Dictionary<Uri, ILighthouseServiceContainer>();
+
+        public IList<NetworkScope> SupportedScopes => new[] { NetworkScope.Local };
+
+        public IList<NetworkProtocol> SupportedProtocols => new[] { NetworkProtocol.TCP };
+
+        public Task<byte[]> GetByteArrayAsync(Uri uri)
         {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetObjectAsync<T>(Uri uri, bool throwErrors = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStringAsync(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> MakeRequest<T>(Uri uri, string content, bool throwErrors = false)
+        {
+            throw new NotImplementedException();
         }
     }
 }
